c0658dbb67767dc08c59992ccdbc44e0aaf789c6
Denis <denis.dimitriy@gmail.com>
2018-03-24 13:11:41 +0200

    release 1.1.1

--- functions.js
+++ functions.js
@@ -1,3 +1,4 @@
+/** Глубокое копирование объектов */
 function copyDeep(donor) {
     var recipient
     var typeString = {}.toString.call(donor);
@@ -21,4 +22,153 @@ function copyDeep(donor) {
             break;
     }
     return recipient
+}
+
+/** Создать элемент позиции */
+function createItemElement(item, list) {
+    var itemElement = document.createElement("div");
+    itemElement.className = "item";
+    
+    var name = document.createElement("input");
+    name.value = item.name;
+    name.className = "name";
+    name.onchange = function (e) {
+      item.name = this.value;
+    }
+
+    var price = document.createElement("input");
+    price.value = item.price;
+    price.className = "price";
+    price.onchange = function (e) {
+      item.price = +this.value;
+      item.result = item.quantity * item.price;
+      result.value = item.result;
+    }
+    
+    var quantity = document.createElement("input");
+    quantity.value = item.quantity;
+    quantity.className = "quantity";
+    quantity.onchange = function (e) {
+      item.quantity = +this.value;
+      item.result = item.quantity * item.price;
+      result.value = item.result;
+    }
+    
+    var result = document.createElement("input");
+    result.value = item.result;
+    result.className = "result";
+    result.onchange = function (e) {
+      item.result = +this.value;
+      item.quantity = item.result / item.price;
+      quantity.value = item.quantity;
+    }
+
+    var remove = document.createElement("button");
+    remove.innerHTML = "удалить";
+    remove.className = "remove-item-btn";
+    remove.onclick = function (e) {
+        var index = list.indexOf(item);
+        list.splice(index, 1);
+        itemElement.parentElement.removeChild(itemElement);
+    }
+
+    var categories = document.createElement("input");
+    categories.value = item.categories.join(", ");
+    categories.className = "categories";
+    categories.onchange = function (e) {
+      item.categories = this.value.split(", ");
+      categories.value = item.categories.join(", ");
+    }
+
+    itemElement.appendChild(name);
+    itemElement.appendChild(categories);
+    itemElement.appendChild(price);
+    itemElement.appendChild(quantity);
+    itemElement.appendChild(result);
+    itemElement.appendChild(remove);
+
+    return itemElement;
+}
+
+/** Создать элемент калькулятора */
+function createCalculatorElement (calculator, list, calculateBtn, results) {
+    
+    var calcElement = document.createElement("div");
+
+    var categoriesElement = document.createElement("input");
+    categoriesElement.value = calculator.categories.join(", ");
+    categoriesElement.onchange = function (e) {
+        calculator.categories = this.value.split(", ");
+        categoriesElement.value = calculator.categories.join(", ");
+    }
+
+    var resultElement = document.createElement("span");
+
+    var removeElement = document.createElement("button");
+    removeElement.innerHTML = "удалить";
+    removeElement.className = "remove-calculator-btn";
+    removeElement.onclick = function (e) {
+        var index = results.indexOf(calculator);
+        results.splice(index, 1);
+        calcElement.parentElement.removeChild(calcElement);
+    }
+
+    calcElement.appendChild(removeElement);
+    calcElement.appendChild(categoriesElement);
+    calcElement.appendChild(resultElement);
+
+    calculateBtn.addEventListener("click", calculate);
+
+    function calculate () {
+        console.log("works");
+        calculator.result = 0;
+        list.forEach(item => {
+            if (matchingCategory(calculator.categories, item.categories)){
+                calculator.result += item.result;
+            }
+        });
+        resultElement.innerHTML = calculator.result;
+    }
+
+    return calcElement;
+}
+
+/** Проверить соответствие массивов категорий */
+function matchingCategory (ruleArray, itemArray) {
+    var matches = 0;
+    for (var i=0; i<ruleArray.length; i++){
+        for(var j=0; j<itemArray.length; j++){
+            if (ruleArray[i] == itemArray[j]){
+                matches++;
+                break;
+            }
+        }
+    }
+    if (matches == ruleArray.length) return true
+}
+
+/** Создать элемент шаблона */
+function createPatternElement (pattern, list, baseListElement) {
+
+    var patternElement = document.createElement("div");
+
+    var nameElement = document.createElement("span");
+    nameElement.innerHTML = pattern.name;
+
+    var priceElement = document.createElement("span");
+    priceElement.innerHTML = pattern.price;
+
+    var addItemBtn = document.createElement("button");
+    addItemBtn.innerHTML = "Добавить";
+    addItemBtn.onclick = function () {
+        var item = new Item(pattern.name, pattern.price, pattern.categories);
+        list.push(item);
+        setListElement.appendChild(createItemElement(item, list));
+    }
+
+    patternElement.appendChild(addItemBtn);
+    patternElement.appendChild(nameElement);
+    patternElement.appendChild(priceElement);
+
+    baseListElement.appendChild(patternElement);
 }
--- script.js
+++ script.js
@@ -1,98 +1,61 @@
-var item1 = new Item();
-var item2 = new Item();
+/**Program
+ * создать структуру данных (набор позиций с данными)
+ * на ее основе создать DOM, описав необходимые обработчики событий для управления структурой данных
+ */
+
+// Инициализация DOM элементов
+var baseListElement = document.getElementById("base-list");
+var setListElement = document.getElementById("set-list");
+var resultListElement = document.getElementById("result-list");
+var addItemBtn = document.getElementById("add-item-btn");
+var addCalculatorBtn = document.getElementById("add-calculator-btn");
+var calculateBtn = document.getElementById("calculate-btn");
 
 
+// Секция набора позиций
+var list = [];
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-//Подключить main
-var mainElement = document.getElementById("main");
-
-//Объект одной позиции
-var item = {};
-
-//Свойства
-item.name = "cabel";
-item.price = 30;
-item.quantity = 0;
-item.result = 0;
-item.element = {};
-
-//Инициализация DOM элемента
-item.element.container = document.createElement("div");
-item.element.container.className = "container";
-
-item.element.name = document.createElement("input");
-item.element.name.value = item.name;
-item.element.name.className = "name";
-
-item.element.quantity = document.createElement("input");
-item.element.quantity.value = item.quantity;
-item.element.quantity.className = "quantity";
-
-item.element.result = document.createElement("input");
-item.element.result.value = item.result;
-item.element.result.className = "result";
-
-item.element.container.appendChild(item.element.name);
-item.element.container.appendChild(item.element.quantity);
-item.element.container.appendChild(item.element.result);
-
-//Добавить элемент в DOM
-mainElement.appendChild(item.element.container);
-
-//Обновить DOM элемент
-item.render = function () {
-    item.element.result.value = item.result;
-    item.element.quantity.value = item.quantity;
-    item.element.name.value = item.name;
-};
-
-//Ввод данных
-item.element.quantity.onchange = function (e) {
-    item.quantity = +item.element.quantity.value
-    item.result = item.quantity * item.price;
-    item.render();
+addItemBtn.onclick = function() {
+    var item = new Item();
+    list.push(item);
+    setListElement.appendChild(createItemElement(item, list));
 }
 
-item.element.result.onchange = function (e) {
-    item.result = +item.element.result.value
-    item.quantity = item.result / item.price;
-    item.render();
+// Примеры
+var item = new Item("кабель", 5, ["все", "материалы", "магистраль"]);
+list.push(item);
+setListElement.appendChild(createItemElement(item, list));
+
+
+item = new Item("патчкорд", 10, ["все", "материалы", "шкаф"]);
+list.push(item);
+setListElement.appendChild(createItemElement(item, list));
+
+
+item = new Item("сплиттер", 15, ["все", "оборудование", "шкаф"]);
+list.push(item);
+setListElement.appendChild(createItemElement(item, list));
+
+
+
+// Секция базы шаблонов
+for (var i=0; i<base.length; i++){
+    var pattern = base[i];
+    createPatternElement (pattern, list, baseListElement)
 }
-*/
+
+
+// Секция результатов расчета
+var results = [];
+
+addCalculatorBtn.onclick = function() {
+    var calculator = new Сalculator ();
+    results.push(calculator);
+    resultListElement.appendChild(createCalculatorElement (calculator, list, calculateBtn, results))
+}
+
+// Пример
+var calculator = new Сalculator (["все"])
+results.push(calculator);
+resultListElement.appendChild(createCalculatorElement (calculator, list, calculateBtn, results))
+
